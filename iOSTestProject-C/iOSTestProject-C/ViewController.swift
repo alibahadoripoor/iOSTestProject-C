7faf9a58ccdda9b4d1c5eeea0e6392db65564016
Ali Bahadoripoor <ali.bahadori@mail.de>
2020-12-24 07:33:28 -0800

    Initial Commit

---
+++ iOSTestProject-C/iOSTestProject-C/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOSTestProject-C
+//
+//  Created by Ali Bahadori on 23.12.20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
